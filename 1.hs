989457b4c02d3e420768cc6761fbc19942c071b4
mniip <mniip@mniip.com>
2022-12-02 06:17:38 +0100

    Remove unnecessary pragmas/imports

--- 1.hs
+++ 1.hs
@@ -1,81 +1,14 @@
-{-# LANGUAGE GHC2021 #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE CUSKs #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DefaultSignatures #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE ExplicitNamespaces #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE ImplicitParams #-}
-{-# LANGUAGE ImpredicativeTypes #-}
-{-# LANGUAGE IncoherentInstances #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE LexicalNegation #-}
-{-# LANGUAGE LiberalTypeSynonyms #-}
-{-# LANGUAGE LinearTypes #-}
-{-# LANGUAGE MagicHash #-}
-{-# LANGUAGE MonadComprehensions #-}
-{-# LANGUAGE MultiWayIf #-}
-{-# LANGUAGE NegativeLiterals #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE NondecreasingIndentation #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE NumDecimals #-}
-{-# LANGUAGE OverloadedLabels #-}
-{-# LANGUAGE OverloadedRecordDot #-}
-{-# LANGUAGE OverloadedRecordUpdate #-}
-{-# LANGUAGE PackageImports #-}
-{-# LANGUAGE ParallelListComp #-}
 {-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE QualifiedDo #-}
-{-# LANGUAGE QuantifiedConstraints #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE RecursiveDo #-}
-{-# LANGUAGE RoleAnnotations #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeFamilyDependencies #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE UndecidableSuperClasses #-}
-{-# LANGUAGE UnicodeSyntax #-}
-{-# LANGUAGE UnliftedNewtypes #-}
-{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wno-partial-type-signatures #-}
 
 module Main where
 
-import AOC.Imports
-import qualified AOC.Imports.BS as BS
-import qualified AOC.Imports.BSC as BSC
-import qualified AOC.Imports.BSL as BSL
-import qualified AOC.Imports.BSLC as BSLC
-import qualified AOC.Imports.Env as Env
-import qualified AOC.Imports.Exception as Exception
-import qualified AOC.Imports.IM as IM
-import qualified AOC.Imports.IS as IS
-import qualified AOC.Imports.Lens as Lens
-import qualified AOC.Imports.M as M
-import qualified AOC.Imports.P as P
-import qualified AOC.Imports.R as R
-import qualified AOC.Imports.S as S
-import qualified AOC.Imports.Seq as Seq
-import qualified AOC.Imports.T as T
-import qualified AOC.Imports.TH as TH
-import qualified AOC.Imports.TL as TL
-import qualified AOC.Imports.Traced as Traced
+import AOC.Common
+import Data.List
+import Data.List.Split
 
 main :: IO ()
 main = aocMain readInput1 solve1 printOutput1 readInput2 solve2 printOutput2
-
-fromFile :: String -> _
-fromFile filename = readInput2 $ unsafePerformIO $ readFile filename
 
 readInput1 :: _ -> [[Int]]
 readInput1 = map (map read . lines) . splitOn "\n\n"
